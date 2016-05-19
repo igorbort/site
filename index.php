43079d3d7a97f4769cdce38d53b8d5fc696303be
igorbort32 <igorbortnikov32@gmail.com>
2016-05-19 20:05:00 +0300

    first commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html> 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
-		<title>Моя PHP сторінка GIT</title>
+		<title>Моя PHP сторінка GITt</title>
 		<link href="style2.css" rel="stylesheet" type="text/css"/>
 	</head>
 
